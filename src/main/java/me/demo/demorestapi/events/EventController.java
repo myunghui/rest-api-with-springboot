6f35ed945bcd00926dfa15b45a12dfac8dda6f24
myunghui <myunghui.jang@gmail.com>
2019-01-16 01:41:54 +0900

    JSON Response 추가

---
+++ src/main/java/me/demo/demorestapi/events/EventController.java
@@ -0,0 +1,30 @@
+package me.demo.demorestapi.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
+public class EventController {
+
+    @PostMapping
+//    @PostMapping("/api/events")
+    public ResponseEntity createEvent(@RequestBody Event event) {
+
+//        URI uri = linkTo(methodOn(EventController.class).createEvent(null)).slash("{id}").toUri();
+        URI uri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+//        return ResponseEntity.created(uri).build();
+        return ResponseEntity.created(uri).body(event);
+
+    }
+
+}
